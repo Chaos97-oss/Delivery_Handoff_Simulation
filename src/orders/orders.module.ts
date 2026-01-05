cef87276d7d94f143176e445fc185f15e3e5531d
Paul <udegbuepaul43@gmail.com>
2026-01-05 02:15:43 +0100

    Order module created

---
+++ src/orders/orders.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+import { Order } from './order.entity';
+import { OrderAssignment } from './order-assignment.entity';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Order, OrderAssignment])],
+    controllers: [OrdersController],
+    providers: [OrdersService]
+})
+export class OrdersModule {}
